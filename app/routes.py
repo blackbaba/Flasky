660e9d36118de82d28f471f623c27230cec23e3c
Amrinder Dhindsa <amrinder.dhindsa1@gmail.com>
2020-09-26 19:33:58 +0800

    add language column to posts model

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from guess_language import guess_language
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.urls import url_parse
@@ -24,7 +25,10 @@ def before_request():
 def index():
     form = PostForm()
     if form.validate_on_submit():
-        post = Post(body=form.post.data, author=current_user)
+        language = guess_language(form.post.data)
+        if language == 'UNKNOWN' or len(language) > 5:
+            language = ''
+        post = Post(body=form.post.data, author=current_user, language=language)
         db.session.add(post)
         db.session.commit()
         flash('Your post is now live!')
