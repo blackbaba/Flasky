b4652ef9a7e445ec0521152bfd28e83be8a852be
Amrinder Dhindsa <amrinder.dhindsa1@gmail.com>
2020-09-26 13:12:36 +0800

    add flask bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -3,13 +3,17 @@ from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_mail import Mail
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
+mail = Mail(app)
+bootstrap=Bootstrap(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
--- config.py
+++ config.py
@@ -7,10 +7,11 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_SERVER = 'smtp.mailtrap.io'
+    MAIL_PORT = 2525
+    MAIL_USERNAME = '6ae01515e64c92'
+    MAIL_PASSWORD = 'c02e04a2bfd8cd'
+    MAIL_USE_TLS = True
+    MAIL_USE_SSL = False
     ADMINS = ['your-email@example.com']
     POSTS_PER_PAGE = 25
